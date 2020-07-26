dbbf48eeb603fab932d876ac3fcf29243f8d16d7
Vantol Bennett <teckcare2011@gmail.com>
2020-07-25 22:00:45 -0500

    Addition of electron
    Addition of electron dependencies and main.js added to configure electron.

---
+++ main.js
@@ -0,0 +1,31 @@
+const { app, BrowserWindow } = require('electron');
+
+let win;
+
+function createWindow() {
+    win = new BrowserWindow({
+        width: 800,
+        height: 600,
+    });
+
+    //win.loadURL('index.html');
+    win.loadURL(`file://${__dirname}/build/index.html`);
+
+    win.on('closed', () => {
+        win = null;
+    });
+}
+
+app.on('ready', createWindow);
+
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') {
+        app.quit();
+    }
+});
+
+app.on('activate', () => {
+    if (win === null) {
+        createWindow()
+    }
+});
